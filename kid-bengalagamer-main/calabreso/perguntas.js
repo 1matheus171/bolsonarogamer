624b6c44a354dee755350d839f3fb64bf0acb195
1matheus171 <matheus.mix211@gmail.com>
2024-10-17 11:32:02 -0300

    Add files via upload

---
+++ kid-bengalagamer-main/calabreso/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'brasil',
+    'qual foi o melhor presidente do brasil?',
+    'bolsonaro'
+)
+
+criaCartao(
+    'brasil',
+    'o que o bolsonaro é denominado?',
+    'mito'
+)
+
+criaCartao(
+    'brasil',
+    'quem é conhecido por fazer o "M"',
+    'pablo marçal'
+)
+
+criaCartao(
+    'EUA',
+    'quem foi o melhor presidente dos EUA?',
+    'Donald Trump'
+)
